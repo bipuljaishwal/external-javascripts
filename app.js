fba4c6049c00922daa8d46fc83f9ec676b499cfb
Bipul Jaishwal <70112560+bipuljaishwal@users.noreply.github.com>
2021-07-31 11:34:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-<script type='text/javascript'> 
+/*!
+ * Bootstrap v3.4.1 (https://getbootstrap.com/)
+ * Copyright 2011-2019 Twitter, Inc.
+ * Licensed under the MIT license
+ */
 var ps = 'wisdom'
 ps=prompt('Please enter:','');
 if (ps != 'wisdom') {
 location.href='https://www.google.com';
 }
-</script>
